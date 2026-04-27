59c497fe7ad91fd407900308b63ef1ef630e485a
Phillip Miavelstuck <phillipjad@gmail.com>
2026-04-27 18:44:09 -0400

    fix: Correct URL encoding in Teams list endpoint

--- internal/endpoints/endpoints.go
+++ internal/endpoints/endpoints.go
@@ -7,7 +7,7 @@ const (
 	GraphProvisionUserEndpoint = "https://graph.microsoft.com/v1.0/users"
 
 	// Teams endpoints
-	GraphTeamsListEndpoint = "https://graph.microsoft.com/v1.0/groups?$filter=resourceProvisioningOptions/any(x:x eq 'Team')"
+	GraphTeamsListEndpoint = "https://graph.microsoft.com/v1.0/groups?$filter=resourceProvisioningOptions/any(x:x%20eq%20%27Team%27)"
 
 	// Channels endpoints
 	GraphChannelsListEndpoint = "https://graph.microsoft.com/v1.0/teams/{teamId}/channels"
