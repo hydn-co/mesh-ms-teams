e8f74fcb0d07635017d506cf30204a353a5b20ca
Phillip Miavelstuck <phillipjad@gmail.com>
2026-05-08 14:57:21 -0400

    chore: update mesh-sdk dependency to v0.2.55 and refactor feature registration

--- cmd/main.go
+++ cmd/main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/hydn-co/mesh-ms-teams/internal/collectors"
 	"github.com/hydn-co/mesh-ms-teams/internal/options"
 	"github.com/hydn-co/mesh-ms-teams/internal/payloads"
+	"github.com/hydn-co/mesh-sdk/pkg/connector"
 	"github.com/hydn-co/mesh-sdk/pkg/runner"
 )
 
@@ -23,52 +24,46 @@ func WithManifest() *runner.Manifest {
 	)
 
 	// Register Teams Collector
-	if err := manifest.RegisterFeature(
+	manifest.MustRegisterFeature(
 		"teams_collector",
 		"Teams Collector",
 		"Collects teams from Microsoft Teams and emits them as catalog entities.",
-		true,
+		runner.FeatureSchedulable,
 		runner.FeatureTypeCollector,
 		new(options.TeamsCollectorOptions),
-		nil,
+		(*connector.NoPayload)(nil),
 		runner.FeatureResumeBehaviorNone,
 		runner.GrantCredential,
 		runner.Factory(collectors.NewTeamsCollector),
-	); err != nil {
-		log.Fatal(err)
-	}
+	)
 
 	// Register Channels Collector
-	if err := manifest.RegisterFeature(
+	manifest.MustRegisterFeature(
 		"channels_collector",
 		"Channels Collector",
 		"Collects channels from a specified Microsoft Teams team and emits them as catalog entities.",
-		true,
+		runner.FeatureSchedulable,
 		runner.FeatureTypeCollector,
 		new(options.ChannelsCollectorOptions),
-		nil,
+		(*connector.NoPayload)(nil),
 		runner.FeatureResumeBehaviorNone,
 		runner.GrantCredential,
 		runner.Factory(collectors.NewChannelsCollector),
-	); err != nil {
-		log.Fatal(err)
-	}
+	)
 
 	// Register Send Message Action
-	if err := manifest.RegisterFeature(
+	manifest.MustRegisterFeature(
 		"send_message_action",
 		"Send Message Action",
 		"Posts messages to Microsoft Teams channels.",
-		false,
+		runner.FeatureUnschedulable,
 		runner.FeatureTypeAction,
 		new(options.SendMessageActionOptions),
 		new(payloads.SendMessagePayload),
 		runner.FeatureResumeBehaviorNone,
 		runner.GrantCredential,
 		runner.Factory(actions.NewSendMessageAction),
-	); err != nil {
-		log.Fatal(err)
-	}
+	)
 
 	if err := manifest.Validate(); err != nil {
 		log.Fatal(err)
